49d753ef0feb3d3e3d6600589f35a0721c023d52
Korek302 <jacek_blady@interia.pl>
2018-05-08 18:40:27 +0200

    zajecia 08.05.18

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _player;
+    private Vector3 _offset;
+
+	void Start ()
+    {
+        _offset = transform.position - _player.position;
+	}
+	
+	void Update ()
+    {
+        transform.position = _player.position + _offset;	
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private float _scale = 0.0f;
+    private float _scaleMax = 2.0f;
+    private float _scaleMin = 1.0f;
+    private bool _direction = true;
+
+	void Update ()
+    {
+        this.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
